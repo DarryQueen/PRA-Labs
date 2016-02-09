84596e1252274b0ebc672d7a81c02c96807e2a0e
DarryQueen <slimfrog@gmail.com>
2016-02-09 14:54:35 +0000

    Initial code commit for lab 2.

---
+++ Lab2/src/narmeendarren/Communication.java
@@ -0,0 +1,177 @@
+package narmeendarren;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+
+public class Communication {
+    private final static int SLEEP_TIME = 1000;
+    private final static String ASCII_FILENAME = "ascii_table.csv";
+    
+    private static Dictionary<String, String> mHexMap = null;
+    
+    private final static String TITLE_DEFAULT = "Rover Camera";
+    private final static String TITLE_SENDING = "Sending...";
+    
+    private static Dictionary<Integer, JComponent> mLabelMap = null;
+    private static JSlider jsCamera;
+    private static JTextArea jtaMessage;
+    private static JButton jbSend;
+    
+    private static Dictionary<String, String> getAsciiTable() {
+        if (mHexMap != null) {
+            return mHexMap;
+        }
+        mHexMap = new Hashtable<String, String>();
+        
+        BufferedReader br = null;
+        String line;
+        try {
+            br = new BufferedReader(new FileReader(ASCII_FILENAME));
+            
+            // We don't care about the header.
+            br.readLine();
+            
+            while ((line = br.readLine()) != null) {
+                String[] cells = line.split(",");
+                mHexMap.put(cells[4], cells[2]);
+            }
+        } catch(Exception e) {}
+        
+        return mHexMap;
+    }
+    
+    private static Dictionary<Integer, JComponent> getLabelMap() {
+        if (mLabelMap != null) {
+            return mLabelMap;
+        }
+        
+        mLabelMap = new Hashtable<Integer, JComponent>();
+        for (int i = 0; i < 10; i++) {
+            mLabelMap.put(i, new JLabel(i + ""));
+        }
+        mLabelMap.put(10, new JLabel("A"));
+        mLabelMap.put(11, new JLabel("B"));
+        mLabelMap.put(12, new JLabel("C"));
+        mLabelMap.put(13, new JLabel("D"));
+        mLabelMap.put(14, new JLabel("E"));
+        mLabelMap.put(15, new JLabel("F"));
+        
+        return mLabelMap;
+    }
+    
+    private static int hexToInt(char h) {
+        switch(h) {
+            case '0': return 0;
+            case '1': return 1;
+            case '2': return 2;
+            case '3': return 3;
+            case '4': return 4;
+            case '5': return 5;
+            case '6': return 6;
+            case '7': return 7;
+            case '8': return 8;
+            case '9': return 9;
+            case 'A': return 10;
+            case 'B': return 11;
+            case 'C': return 12;
+            case 'D': return 13;
+            case 'E': return 14;
+            case 'F': return 15;
+            default: return -1;
+        }
+    }
+    
+    private static int[] stringToHex(String s) {
+        List<Integer> positions = new LinkedList<Integer>(); 
+        for (int i = 0; i < s.length(); i++) {
+            String c = s.charAt(i) + "";
+            String h = getAsciiTable().get(c);
+            
+            // Prepend a 0 if it the hex is less than 16:
+            if (h.length() < 2) {
+                h = "0" + h;
+            }
+            
+            for (int j = 0; j < h.length(); j++) {
+                char ch = h.charAt(j);
+                positions.add(hexToInt(ch));
+            }
+        }
+        
+        int[] positionsArray = new int[positions.size()];
+        for (int i = 0; i < positions.size(); i++) {
+            positionsArray[i] = positions.get(i);
+        }
+        return positionsArray;
+    }
+    
+    public static void moveCamera(int[] positions) {
+        for (int i : positions) {
+            if (i < 16) {
+                jsCamera.setValue(i);
+                try {
+                    Thread.sleep(SLEEP_TIME);
+                } catch(Exception e) {}
+            }
+        }
+    }
+    
+    public static void main(String args[]) {
+        getAsciiTable();
+        
+        // JFrame setup:
+        JFrame myFrame = new JFrame(TITLE_DEFAULT);
+        myFrame.setSize(600, 200);
+        myFrame.setLayout(new BorderLayout());
+        
+        jsCamera = new JSlider(0, 15);
+        jsCamera.setLabelTable(getLabelMap());
+        jsCamera.setPaintLabels(true);
+        myFrame.add(jsCamera, BorderLayout.NORTH);
+        
+        jtaMessage = new JTextArea();
+        myFrame.add(jtaMessage);
+        
+        jbSend = new JButton("Send");
+        jbSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {                
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+                        // Change title, clear text:
+                        myFrame.setTitle(TITLE_SENDING);
+                        String message = jtaMessage.getText();
+                        jtaMessage.setText(null);
+                        
+                        int[] positions = stringToHex(message);
+                        moveCamera(positions);
+                        
+                        // Set the title back:
+                        myFrame.setTitle(TITLE_DEFAULT);
+                    }
+                };
+                
+                new Thread(r).start();
+            }
+        });
+        myFrame.add(jbSend, BorderLayout.SOUTH);
+        
+        myFrame.setVisible(true);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
