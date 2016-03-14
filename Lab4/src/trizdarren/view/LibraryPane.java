222a95467d03e591fcea92e41b507ef842038acc
DarryQueen <slimfrog@gmail.com>
2016-03-14 19:02:39 +0000

    UI with dropdowns. Ugly code, but works for now.

--- Lab4/src/trizdarren/view/LibraryPane.java
+++ Lab4/src/trizdarren/view/LibraryPane.java
@@ -1,17 +1,57 @@
 package trizdarren.view;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class LibraryPane extends JFrame {
+    public static final String SORT_TITLE = "Title";
+    public static final String SORT_YEAR = "Release Year";
+    public static final String SORT_QUALITY = "Quality";
+    public static final String SORT_TRACKNAME = "Track Name";
+    public static final String SORT_ARTIST = "Artist";
+
     private JPanel jpMoviePanel;
     private JPanel jpMusicPanel;
     private JPanel jpOtherPanel;
+
+    private String[] movieSortOptions = {SORT_TITLE, SORT_YEAR, SORT_QUALITY};
+    private String[] musicSortOptions = {SORT_TRACKNAME, SORT_ARTIST};
+    private JComboBox<String> jcbMovieSort;
+    private JComboBox<String> jcbMusicSort;
+
+    private void setUI() {
+        JPanel topPanel = new JPanel(); topPanel.setLayout(new BorderLayout());
+        JPanel topHeader = new JPanel(); topHeader.setLayout(new BorderLayout());
+        topHeader.add(new JLabel("Films"), BorderLayout.WEST);
+        topHeader.add(jcbMovieSort, BorderLayout.EAST);
+        topPanel.add(topHeader, BorderLayout.NORTH);
+        topPanel.add(new JScrollPane(jpMoviePanel), BorderLayout.CENTER);
+
+        JPanel middlePanel = new JPanel(); middlePanel.setLayout(new BorderLayout());
+        JPanel middleHeader = new JPanel(); middleHeader.setLayout(new BorderLayout());
+        middleHeader.add(new JLabel("Music"), BorderLayout.WEST);
+        middleHeader.add(jcbMusicSort, BorderLayout.EAST);
+        middlePanel.add(middleHeader, BorderLayout.NORTH);
+        middlePanel.add(new JScrollPane(jpMusicPanel), BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(); bottomPanel.setLayout(new BorderLayout());
+        JPanel bottomHeader = new JPanel(); bottomHeader.setLayout(new BorderLayout());
+        bottomHeader.add(new JLabel("Unclassified"), BorderLayout.WEST);
+        bottomPanel.add(bottomHeader, BorderLayout.NORTH);
+        bottomPanel.add(new JScrollPane(jpOtherPanel), BorderLayout.CENTER);
+
+        this.add(topPanel);
+        this.add(middlePanel);
+        this.add(bottomPanel);
+    }
 
     public LibraryPane(ActionListener al) {
         super("Media Library");
@@ -19,9 +59,14 @@ public class LibraryPane extends JFrame {
         this.setSize(500, 800);
         this.setLayout(new GridLayout(0, 1));
 
-        jpMoviePanel = new JPanel(); this.add(new JScrollPane(jpMoviePanel));
-        jpMusicPanel = new JPanel(); this.add(new JScrollPane(jpMusicPanel));
-        jpOtherPanel = new JPanel(); this.add(new JScrollPane(jpOtherPanel));
+        jpMoviePanel = new JPanel();
+        jpMusicPanel = new JPanel();
+        jpOtherPanel = new JPanel();
+
+        jcbMovieSort = new JComboBox<String>(movieSortOptions);
+        jcbMusicSort = new JComboBox<String>(musicSortOptions);
+
+        setUI();
     }
 
     private void setPane(JPanel pane, List<MediaPane.Item> itemList) {
